7d801880aac7df5bbd5ca669a3e07b1e5006c6ac
Leandro Lopes <97458494+sRlopEZzz@users.noreply.github.com>
2026-02-21 00:17:47 +0000

    refactor: allow server to run without database in development mode

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 import app from "./src/app.js";
 import dotenv from "dotenv";
 import sequelize from "./src/database/conection.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+async function startServer() {
+  try {
 
-async function startserver(){
-
-  try{
+    // TENTA ligar à BD (mas não trava servidor se falhar)
     await sequelize.authenticate();
-    console.log("ligacao a base de dados estabelecida com sucesso.")
+    console.log("Ligação à base de dados estabelecida com sucesso.");
 
-    app.listen(PORT, () => {
-  console.log(`Servidor a correr na porta ${PORT}`);
-});
-
-  }catch (error){
-    console.log("erro ao ligar a base de dados", error)
+  } catch (error) {
+    console.log("Base de dados ainda não configurada. Servidor continua em modo dev.");
   }
 
+  app.listen(PORT, () => {
+    console.log(`Servidor a correr na porta ${PORT}`);
+  });
 }
 
-
-startserver();
+startServer();
--- src/routes/privateRoutes.js
+++ src/routes/privateRoutes.js
@@ -4,6 +4,6 @@ import AuthController from "../controllers/AuthController.js";
 import auth from "../middlewares/auth.js";
 
 const router = Router();
-router.get("/admin/users", authMiddleware, AdminController.listarUsuarios)
+
 
 export default router;
