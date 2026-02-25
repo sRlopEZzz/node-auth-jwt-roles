f0dac0fdfcdcf79f1f9a2ee2ef5968a810b976d9
Leandro Lopes <97458494+sRlopEZzz@users.noreply.github.com>
2026-02-25 11:54:29 +0000

    feat: implement user registration with email validation token

--- src/controllers/RegisterController.js
+++ src/controllers/RegisterController.js
@@ -1,13 +1,50 @@
-// ypass dev sem banco de dados (teste =
-if(config.DEV_BYPASS_AUTH === "true") {
-    // regra simples para simular roles
-    const tipo_usuario = email.toLowerCase().includes("admin") ? "admin" : "cliente";
-    const token = generateAuthToken({ email, tipo_usuario });
+import bcrypt from "bcryptjs";
+import Usuario from "../models/Usuario.js";
+import { generateEmailToken } from "../utils/jwt.js";
 
-    return res.status(200).json({
-      message: "Login DEV (bypass) efetuado.",
-      token,
-      email,
-      tipo_usuario,
-    });
-  }
+const RegisterController = {    
+    async register(req, res) {
+        try {
+            const { email, senha } = req.body;
+
+            if(!email || !senha) {
+                return res.status(400).json({ error: "Email e senha são obrigatórios." });
+            }
+
+            //verifica se ja existe um user com o email
+            const existente = await Usuario.findByPk(email);
+            if(existente) {     
+                return res.status(409).json({ error: "Email já registrado." });
+            }
+
+            //hash da senha
+            const hashedPassword = await bcrypt.hash(senha, 10);
+
+            //criar utilizador
+            const novoUsuario = await Usuario.create({
+                email,
+                senha: hashedPassword,
+                tipo_usuario: "cliente", //padrão para novos registros
+                email_validado: false, //email ainda não validado
+            });
+
+            //gerar token de validação de email
+            const emailToken = generateEmailToken({ email: novoUsuario.email });
+// Link de validação
+             const link = `http://localhost:3000/api/public/validar-email?token=${token}`;
+
+            
+             // Simulação de envio de email (por agora)
+             console.log("====================================");
+             console.log("LINK DE VALIDACAO EMAIL:");
+             console.log(link);
+             console.log("====================================");
+
+             return res.status(201).json({ message: "Usuário registrado com sucesso. Verifique seu email para validar a conta." });
+} catch (error) {   
+    console.error("Erro no registro:", error);
+    return res.status(500).json({ error: "Erro no servidor." });        
+                        }
+            },
+};
+export default RegisterController;
