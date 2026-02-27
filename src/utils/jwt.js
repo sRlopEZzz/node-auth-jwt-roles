7e20507ac05223ceca1aad992b954e049af787cd
Leandro Lopes <97458494+sRlopEZzz@users.noreply.github.com>
2026-02-27 14:47:43 +0000

    fix(jwt): correct auth secret reference

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -6,13 +6,12 @@ const AUTH_EXPIRES = process.env.JWT_AUTH_EXPIRES || "1d"; // 1 dia para expirar
 const EMAIL_SECRET = process.env.JWT_EMAIL_SECRET || "dev_email_secret";
 const EMAIL_EXPIRES = process.env.JWT_EMAIL_EXPIRES || "15m"; //15m para ativar a conta (validar email)
 
-
 export function generateAuthToken(payload) {
-  return jwt.sign(payload, AUTH_SECRET, { expiresIn: AUTH_EXPIRES });
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: AUTH_EXPIRES });
 }
 
 export function verifyAuthToken(token) {
-  return jwt.verify(token, AUTH_SECRET);
+  return jwt.verify(token, JWT_SECRET);
 }
 
 //geral tokenn
